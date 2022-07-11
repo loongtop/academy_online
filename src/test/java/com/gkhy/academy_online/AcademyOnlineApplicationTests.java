b723a3721ecc4bd59667615557d86e3dd6fc3e03
leo <loongtop@gmail.com>
2022-07-11 09:29:03 -0400

    first edition

---
+++ src/test/java/com/gkhy/academy_online/AcademyOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkhy.academy_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
